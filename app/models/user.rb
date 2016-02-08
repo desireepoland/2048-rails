a28c7af9c64bf9b59158f989f41a33d9cd095455
Amy Hunter <amyrhunter@gmail.com>
2016-02-08 15:46:46 -0800

    Correct typo in nav file and add some methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_omniauth(auth_hash)
+  # Find or create a user
+  end
+
+  def self.create_from_omniauth(auth_hash)
+  # Create a user
+  end
 end
