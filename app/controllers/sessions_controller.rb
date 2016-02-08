2e27c566426e9de6057326e3e282123f83f5b97d
Desiree <desireepoland@gmail.com>
2016-02-08 15:34:44 -0800

    Add login and log out routes, add to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+
   def create
     auth_hash = request.env['omniauth.auth']
+    if auth_hash["uid"]
+      @user = User.find_or_create_from_omniauth(auth_hash)
+      if @user
+        session[:user_id] = @user.id
+      else
+        flash[:notice] = "Failed to save the user"
+      end
+    else
+      flash[:notice] = "Failed to authenticate"
+    end
+    redirect_to root_path
+  end
+
+  def new; end
+
+  def destroy
+    session[:user_id] = nil
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "site#index"
   get '/auth/:provider/callback', to: 'sessions#create'
+  get "/login" => "sessions#new", as: :login
+  delete "/logout/" => "sessions#destroy", as: :logout
 end
