5bc3578605f2a5a636b72a4b56e4a46ddce72b49
Desiree <desireepoland@gmail.com>
2016-02-09 14:36:25 -0800

    Change game_state in the game model from a string to a text

---
+++ db/migrate/20160209223158_change_games_game_state_from_string_to_text.rb
@@ -0,0 +1,4 @@
+class ChangeGamesGameStateFromStringToText < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209201639) do
+ActiveRecord::Schema.define(version: 20160209223158) do
 
   create_table "games", force: :cascade do |t|
     t.string   "game_state"
