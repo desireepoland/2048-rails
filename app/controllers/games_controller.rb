d1f6004c1c6f1bd5888c5650d47b811cc03ee5b2
Amy Hunter <amyrhunter@gmail.com>
2016-02-09 16:41:57 -0800

    Begin to add controller methods for api endpoints

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def create
+      @game = Game.new(title: params[:story][:title])
+      if @game.save
+        # pass an array of acceptable formats - [:json]
+        render 'show', formats: [:json], handlers: [:jbuilder], status: 201
+      else
+        render json: {error: "Story could not be created."}, status: 422
+      end
+    end
 end
