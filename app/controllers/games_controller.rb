d89c438004097c0befa239a00eea22d6c3cc48b5
Amy Hunter <amyrhunter@gmail.com>
2016-02-09 16:55:02 -0800

    More additions to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,7 +11,25 @@ class GamesController < ApplicationController
         # pass an array of acceptable formats - [:json]
         render 'show', formats: [:json], handlers: [:jbuilder], status: 201
       else
-        render json: {error: "Story could not be created."}, status: 422
+        render json: {error: "Game could not be created."}, status: 422
+      end
+  end
+
+  def update
+        @game = Game.find(params[:id])
+        if @game.update_attributes(something: params[:another][:onemore])
+          render 'show', formats: [:json], handlers: [:jbuilder], status: 200
+        else
+          render json: {error: "Game could not be updated."}, status: 422
+        end
+  end
+
+  def destroy
+      @game = Game.find(params[:id])
+      if @game.destroy
+        render json: {}, status: 200
+      else
+        render json: {error: "Game could not be deleted."}, status: 422
       end
     end
 end
