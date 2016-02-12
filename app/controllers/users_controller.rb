fe886e0c7c7c6bfaa9bf336058f65dade7e09d00
Amy Hunter <amyrhunter@gmail.com>
2016-02-12 14:02:06 -0800

    Styling for table on leaderboard page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def leaderboard
     @users = User.all
     @games = Game.all
-    @top_games = Game.order(score: :desc).limit(20)
+    @top_games = Game.order(score: :desc).limit(5)
   end
 
 end
