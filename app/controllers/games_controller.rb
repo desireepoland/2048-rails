5a80948790244ed69bc5a51af3f132be14f802e6
Desiree <desireepoland@gmail.com>
2016-02-12 09:25:54 -0800

    Remove unneccessary instance variable symbols

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,14 +2,14 @@ class GamesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @games = @current_user.games
+    @games = current_user.games
   end
 
   def show
     if params[:id]
-      @game = @current_user.games.find_by(id: params[:id])
+      @game = current_user.games.find_by(id: params[:id])
     else
-      @game = @current_user.games.first
+      @game = current_user.games.first
     end
     # render json: @game
     render :json => @game.as_json, :status => :ok
@@ -17,12 +17,12 @@ class GamesController < ApplicationController
 
   def new
       # binding.pry
-    @game = @current_user.games.new(game_params)
+    @game = current_user.games.new(game_params)
   end
 
   def create
 
-    @game = @current_user.games.new(game_params)
+    @game = current_user.games.new(game_params)
 
     if @game.save
       # pass an array of acceptable formats - [:json]
